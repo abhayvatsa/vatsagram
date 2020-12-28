6d7086233755bcac95af4dddbb1a95539a0c5fb7
abhayvatsa <atvatsa@gmail.com>
2020-12-28 13:33:38 -0800

    Removing global

--- icons/InfoIcon.tsx
+++ icons/InfoIcon.tsx
@@ -1,28 +1,23 @@
 export default () => (
-  <>
-    <style jsx global>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#e3e3e3"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-info"
+  >
+    <style jsx>
       {`
-        #info {
-          visibility: hidden;
-        }
+        visibility: hidden;
       `}
     </style>
-    <svg
-      id="info"
-      xmlns="http://www.w3.org/2000/svg"
-      width="40"
-      height="40"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="#e3e3e3"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-info"
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-      <line x1="12" y1="16" x2="12" y2="12"></line>
-      <line x1="12" y1="8" x2="12.01" y2="8"></line>
-    </svg>
-  </>
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="12" y1="16" x2="12" y2="12"></line>
+    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+  </svg>
 )
