3eb8fb1d01084a3f8daaac263538ed061b98218e
abhayvatsa <atvatsa@gmail.com>
2020-12-27 16:07:17 -0800

    Use the styled-jsx webpack loader instead. See: https://github.com/vercel/styled-jsx#styles-in-regular-css-files

--- styles/typography.ts
+++
@@ -1,9 +0,0 @@
-import css from 'styled-jsx/css'
-
-export default css.global`
-  html,
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-`
